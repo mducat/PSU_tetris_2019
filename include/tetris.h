49d894a7b83042dede2ac6a367e03a34888af8bf
Mathias Ducatillon <mathias5.ducatillon@epitech.eu>
2020-03-28 23:34:09 +0100

    [~] Correct help message #2

--- include/tetris.h
+++ include/tetris.h
@@ -12,25 +12,25 @@
 #include <getopt.h>
 
 #define USAGE ""                                                        \
-    "Usage: %s [options]\n"                                             \
+    "Usage:  %s [options]\n"                                            \
     "Options:\n"                                                        \
-    " --help\t\t\tDisplay this help\n"                       \
-    " -L --level={num}\tStart Tetris at level num (def: 1)\n"      \
-    " -l --key-right={K}\tMove the tetrimino LEFT using the K key"   \
+    " --help\t\t\tDisplay this help\n"                                  \
+    " -L --level={num}\tStart Tetris at level num (def: 1)\n"           \
+    " -l --key-right={K}\tMove the tetrimino LEFT using the K key"      \
     " (def: left arrow)\n"                                              \
-    " -r --key-right={K}\tMove the tetrimino RIGHT using the K key"  \
+    " -r --key-right={K}\tMove the tetrimino RIGHT using the K key"     \
     " (def: right arrow)\n"                                             \
-    " -t --key-turn={K}\tTURN the tetrimino clockwise 90d using"    \
+    " -t --key-turn={K}\tTURN the tetrimino clockwise 90d using"        \
     " the K key (def: top arrow)\n"                                     \
-    " -d --key-drop={K}\tDROP the tetrimino using the K key (def: " \
+    " -d --key-drop={K}\tDROP the tetrimino using the K key (def: "     \
     "down arrow)\n"                                                     \
-    " -q --key-quit={K}\tQUIT the game using the K key (def: "      \
+    " -q --key-quit={K}\tQUIT the game using the K key (def: "          \
     "'q' key)\n"                                                        \
-    " -p --key-pause={K}\tPAUSE/RESTART the game using the K key"    \
+    " -p --key-pause={K}\tPAUSE/RESTART the game using the K key"       \
     " (def: space bar)\n"                                               \
     " --map-size={row, col}\tSet the numbers of rows and columns of"    \
     " the map (def: 20, 10)\n"                                          \
-    " -w --without-next\tHide next tetrimino (def: false)\n"        \
+    " -w --without-next\tHide next tetrimino (def: false)\n"            \
     " -D --debug\t\tDebug mode (def: false)\n"
 
 #define CPS (10000)
